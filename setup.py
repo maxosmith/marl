27d5cd9bd079a94d23f37e63002aca2d83c77595
Max Smith <max.olan.smith@gmail.com>
2022-12-16 17:13:00 -0500

    Remove compiled files.

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 """Install script for setuptools."""
 from __future__ import absolute_import, division, print_function
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="marl",
     version="0.0.1",
     description="Multiagent Reinforcement Learning building blocks.",
     author="Max Smith",
-    packages=["marl", "marl_experiments"],
+    packages=find_packages(),
     classifiers=[
         "Environment :: Console",
         "Intended Audience :: Science/Research",
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS :: MacOS X",
-        "Programming Language :: Python :: 3.7.5",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
