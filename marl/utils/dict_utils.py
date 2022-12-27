412e72ff62bc3a05089feb39dac4b3ceca7eaf31
Max Smith <max.olan.smith@gmail.com>
2022-12-27 08:13:04 -0500

    Generalize dict operations and unprefix keys.

--- marl/utils/dict_utils.py
+++ marl/utils/dict_utils.py
@@ -1,6 +1,24 @@
-from typing import Any, Mapping
+from typing import Any, Callable, Mapping
 
 
-def prefix_keys(x: Mapping[str, Any], prefix: str) -> Mapping[str, Any]:
+def key_apply(x: Mapping[Any, Any], fn: Callable[[Any], Any]) -> Mapping[Any, Any]:
+    """Apply function over keys."""
+    return {fn(k): v for k, v in x.items()}
+
+
+def prefix_keys(x: Mapping[str, Any], prefix: str, delimiter: str = "/") -> Mapping[str, Any]:
     """Apply a prefix to all dictionary keys."""
-    return {f"{prefix}{k}": v for k, v in x.items()}
+    return key_apply(x, lambda k: f"{prefix}{delimiter}{k}")
+
+
+def unprefix_keys(x: Mapping[str, Any], prefix: str, delimiter: str = "/") -> Mapping[str, Any]:
+    """Remove a prefix from all dictionary keys."""
+
+    def _unprefix(k: str) -> str:
+        """Remove a prefix from a key."""
+        # This is a slower unprefix method to validate inputs.
+        tokens = k.split(delimiter)
+        assert (len(tokens) >= 2) and (tokens[0] == prefix)
+        return delimiter.join(tokens[1:])
+
+    return key_apply(x, _unprefix)
--- marl/utils/dict_utils_test.py
+++ marl/utils/dict_utils_test.py
@@ -1,21 +1,38 @@
-"""Test cases for the `tree_utils` module."""
+"""Test cases for the `dict_utils` module."""
 from absl.testing import absltest, parameterized
 
 from marl.utils import dict_utils, tree_utils
 
 
 class DictUtilsTest(parameterized.TestCase):
+    """Test cases for dictionary utility functions."""
+
     @parameterized.parameters(
         [
             dict(
                 input={0: 1.0, 1: 2.0},
                 prefix="a",
-                output={"a0": 1.0, "a1": 2.0},
+                output={"a/0": 1.0, "a/1": 2.0},
             ),
         ]
     )
     def test_prefix_keys(self, input, prefix, output):
+        """Test prefixing keys."""
         result = dict_utils.prefix_keys(input, prefix)
+        tree_utils.assert_equals(result, output)
+
+    @parameterized.parameters(
+        [
+            dict(
+                input={"a/0": 1.0, "a/1": 2.0},
+                prefix="a",
+                output={"0": 1.0, "1": 2.0},
+            ),
+        ]
+    )
+    def test_unprefix_keys(self, input, prefix, output):
+        """Test removing prefix from keys."""
+        result = dict_utils.unprefix_keys(input, prefix)
         tree_utils.assert_equals(result, output)
 
 
