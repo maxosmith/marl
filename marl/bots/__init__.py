c17be0a102c64d168dd0f7dcf587944d76af8375
Max Smith <max.olan.smith@gmail.com>
2023-09-19 10:53:30 -0400

    Random action bot.

--- marl/bots/__init__.py
+++ marl/bots/__init__.py
@@ -1,7 +1,9 @@
 from .action_sequence_bot import ActionSequenceBot
 from .constant_action_bot import ConstantActionBot
+from .random_action_bot import RandomActionBot
 
 __all__ = (
     "ActionSequenceBot",
     "ConstantActionBot",
+    "RandomActionBot",
 )
