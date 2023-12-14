9bef96e4fb2c8485a0ecc01ef32e694b9c2129da
Max Smith <max.olan.smith@gmail.com>
2023-12-14 12:33:29 -0500

    Random action bot compat with OpenSpiel.

--- marl/bots/random_action_bot.py
+++ marl/bots/random_action_bot.py
@@ -1,9 +1,20 @@
 """Bot that randomly selects actions."""
+import dataclasses
 from typing import Tuple
 
 import numpy as np
 
 from marl import individuals, types, worlds
+
+
+@dataclasses.dataclass
+class AgentState:
+  """Agent state for `RandomActionBot`.
+
+  Logits are included in its state to allow for Fake testing of stochastic policies.
+  """
+
+  logits: np.ndarray
 
 
 class RandomActionBot(individuals.Bot):
@@ -13,6 +24,7 @@ class RandomActionBot(individuals.Bot):
     """Initializer."""
     self._num_actions = num_actions
     self._rng = np.random.default_rng(seed)
+    self._state = AgentState(logits=np.ones((self._num_actions), dtype=float) / num_actions)
 
   def step(
       self,
@@ -20,10 +32,16 @@ class RandomActionBot(individuals.Bot):
       timestep: worlds.TimeStep,
   ) -> Tuple[types.Action, types.State]:
     """Selects an action to take given the current timestep."""
-    del timestep
-    return state, self._rng.choice(self._num_actions)
+    del state
+    if isinstance(timestep.observation, dict) and ("legal_actions" in timestep.observation):
+      action_probs = timestep.observation["legal_actions"].astype(float)
+      action_probs /= np.sum(action_probs)
+      possible_actions = np.arange(timestep.observation["legal_actions"].shape[0])
+      return AgentState(logits=action_probs), self._rng.choice(possible_actions, p=action_probs)
+    else:
+      return self._state, self._rng.choice(self._num_actions)
 
   def episode_reset(self, timestep: worlds.TimeStep) -> types.State:
     """Resets the agent's episodic state."""
     del timestep
-    return ()
+    return self._state
