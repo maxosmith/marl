7eff913069c92542e069d03c9f1c12b47da4550b
Max Smith <max.olan.smith@gmail.com>
2024-01-15 13:32:34 -0500

    Eval & Sim added to arena API.

--- marl/services/arenas/__init__.py
+++ marl/services/arenas/__init__.py
@@ -1,7 +1,12 @@
+"""Arenas for simulating players."""
 from .base_arena import BaseArena
+from .eval_arena import EvalArena
+from .sim_arena import SimArena
 from .train_arena import TrainArena
 
 __all__ = (
     "BaseArena",
     "TrainArena",
+    "EvalArena",
+    "SimArena",
 )
