2d2e4655faa1f0e1904601e839bedf7921946777
Max Smith <max.olan.smith@gmail.com>
2024-01-15 13:20:58 -0500

    Eval arena sequential games.

--- marl/services/arenas/eval_arena.py
+++ marl/services/arenas/eval_arena.py
@@ -1,15 +1,16 @@
 """Interface for servies that simulate episodes."""
 import dataclasses
-import operator
+import time
 from typing import Any, Callable, Mapping, NamedTuple, Optional, Sequence, Union
 
 import numpy as np
 import tree
 from absl import logging
 
-from marl import types, worlds
+from marl import individuals, types, worlds
+from marl.services import snapshotter as snap_lib
 from marl.services.arenas import base_arena
-from marl.utils import dict_utils, loggers, tree_utils
+from marl.utils import dict_utils, loggers, signals, tree_utils
 
 
 class EpisodeResult(NamedTuple):
@@ -42,104 +43,129 @@ class EvaluationScenario:
 
 @dataclasses.dataclass
 class EvalArena(base_arena.BaseArena):
-  """Arena for periodic evaluation of a learner."""
+  """Arena for periodic evaluation of a learner.
 
+  Args:
+    snapshotter: Service for saving an evaluated learner's parameters.
+    snapshotter_scenario_id: Scenario ID used to measure snapshot priorities.
+    learner_id: Player ID to snapshot.
+  """
+
+  players: Mapping[types.PlayerID, individuals.Bot | individuals.Agent]
   scenarios: Union[EvaluationScenario, Sequence[EvaluationScenario]]
-  logger: Optional[Any] = None
-  counter: Optional[Any] = None
-  step_key: Optional[str] = None
+  counter: Any
+  step_key: str
+  evaluation_frequency: int = 1
+  logger: Any | None = None
+  snapshotter: snap_lib.PrioritySnapshotter | None = None
+  snapshotter_scenario_id: int | None = None
+  learner_id: int | None = None
 
   def __post_init__(self):
-    if self.counter and not self.step_key:
-      raise ValueError("Must specify step key with counter.")
-    if self.step_key and not self.counter:
-      raise ValueError("Must specify counter with step key.")
-
+    """Post initializer."""
     if isinstance(self.scenarios, EvaluationScenario):
       self.scenarios = [self.scenarios]
-
     self._running = False
+    self._stop = False
+    self._last_eval = -1
 
-  def run_evaluation(self, players: Any, step: Optional[int] = None):
+  def run_evaluation(self, step: Optional[int] = None):
     logging.info("Running evaluation scenarios.")
     # Get the current state of all learning agents.
-    for agent in players.values():
+    for agent in self.players.values():
       agent.update()
 
     evaluation_results = []
 
-    for scenario in self.scenarios:
+    for scenario_i, scenario in enumerate(self.scenarios):
       if scenario.name:
-        logging.info(f"\tRunning scenario: {scenario.name}")
+        logging.info("\tRunning scenario: %s", scenario.name)
 
       game = scenario.game_ctor(**scenario.game_kwargs)
 
-      results = [self._run_episode(game, players) for _ in range(scenario.num_episodes)]
-      results = tree.map_structure(lambda *args: scenario.aggregate_fn([args]), *results)
+      results = [self._run_episode(game, self.players) for _ in range(scenario.num_episodes)]
+      results = tree.map_structure(lambda *args, scenario=scenario: scenario.aggregate_fn([args]), *results)
       evaluation_results.append(results)
 
       if self.logger:
         results = results.to_logdata()
         if step:
-          results[self._step_key] = step
+          results[self.step_key] = step
         if scenario.name:
           results = dict_utils.prefix_keys(results, scenario.name)
         self.logger.write(results)
+
+      if self.snapshotter and (scenario_i == self.snapshotter_scenario_id):
+        self.snapshotter.save(results[f"episode_return/player_{self.learner_id}"], self.players[self.learner_id].params)
 
     logging.info("Evaluation complete.")
     return evaluation_results
 
   def _run_episode(self, game, players) -> EpisodeResult:
+    """Run a single evaluation episode."""
     timesteps = game.reset()
-    # Reset player's episodic state, and likely force sync their parameters.
-    player_states = {id: player.episode_reset(timesteps[id]) for id, player in players.items()}
+    player_states = {id: None for id in players.keys()}
+
+    has_finished = {id: False for id in players.keys()}
+    for player_id, timestep in timesteps.items():
+      if timestep.last():
+        has_finished[player_id] = True
 
     # Initialize logging statistics.
     episode_length = 0
-    episode_return = {id: ts.reward for id, ts in timesteps.items()}
+    episode_return = {id: 0.0 for id in players.keys()}
+    for player_id, timestep in timesteps.items():
+      episode_return[player_id] += timestep.reward
 
-    while not np.all([ts.last() for ts in timesteps.values()]):
+    while not np.all(list(has_finished.values())):
       # Action selection.
       actions = {}
-      for id, player in players.items():
-        player_states[id], actions[id] = player.step(player_states[id], timesteps[id])
+      for player_id, timestep in timesteps.items():
+        if player_states[player_id] is None:
+          # Player's first timetep: episodic state, and likely force sync their parameters.
+          player_states[player_id] = players[player_id].episode_reset(timestep)
+        player_states[player_id], actions[player_id] = players[player_id].step(player_states[player_id], timestep)
 
       # Transition game state.
       timesteps = game.step(actions)
       episode_length += 1
-      episode_return = tree.map_structure(
-          operator.iadd,
-          episode_return,
-          {id: ts.reward for id, ts in timesteps.items()},
-      )
-
-      # Maybe sync agent parameters.
-      for player in players.values():
-        player.update()
+      for player_id, timestep in timesteps.items():
+        episode_return[player_id] += timestep.reward
+        if timestep.last():
+          has_finished[player_id] = True
 
     return EpisodeResult(
         episode_length=episode_length,
         episode_return=episode_return,
     )
 
-  def run(
-      self,
-      *,
-      num_episodes: Optional[int] = None,
-      num_timesteps: Optional[int] = None,
-      **kwargs,
-  ):
-    """Run the arena to generate experience.
+  def run(self, *, num_episodes: Optional[int] = None, num_timesteps: Optional[int] = None, **kwargs):
+    """Periodically run all evaluation scenarios."""
+    del num_episodes, num_timesteps, kwargs
+    self._running = True
+    with signals.runtime_terminator():
+      while True:
+        if self._stop:  # Check if the services was manually stopped.
+          self._stop = False
+          break
 
-    Runs either `num_episodes`, or _at least_ `num_timesteps`, or indefinitely.
+        # Check the current step.
+        counts = self.counter.get_counts()
+        step = counts.get(self.step_key, 0)
 
-    Args:
-      num_episodes: Number of episodes to run.
-      num_timesteps: Minimum number of timesteps to run.
-    """
-    pass
+        if step >= (self._last_eval + self.evaluation_frequency):
+          self.run_evaluation(step)
+          self._last_eval = step
+
+        # Don't spam the counter.
+        for _ in range(10):
+          # Do not sleep for a long period of time to avoid LaunchPad program
+          # termination hangs (time.sleep is not interruptible).
+          time.sleep(1)
+    self._running = False
 
   def stop(self):
     """Stop running this service if set to run indefinitely."""
     if not self._running:
       raise RuntimeError("Tried to stop an arena that isn't running.")
+    self._stop = True
--- marl/services/arenas/eval_arena_test.py
+++ marl/services/arenas/eval_arena_test.py
@@ -1,6 +1,10 @@
 """Tests for eval_arena."""
+import numpy as np
 from absl.testing import absltest, parameterized
+from psro.test_utils import ScriptGame
 
+from marl import bots, worlds
+from marl.games import openspiel_proxy
 from marl.services.arenas import eval_arena, test_utils
 from marl.utils import tree_utils
 
@@ -34,16 +38,78 @@ class EvalArenaTest(parameterized.TestCase):
 
     scenario = eval_arena.EvaluationScenario(
         game_ctor=test_utils.MockGame,
-        game_kwargs=dict(trajectory=timesteps, expected_actions=actions),
+        game_kwargs={"trajectory": timesteps, "expected_actions": actions},
         num_episodes=2,
     )
     arena = eval_arena.EvalArena(
+        players=players,
         scenarios=scenario,
+        counter=None,
+        step_key=None,
         logger=logger,
     )
 
-    results = arena.run_evaluation(players)
-    tree_utils.assert_equals(results, expected_results)
+    results = arena.run_evaluation()
+    tree_utils.assert_equals(expected_results, results)
+
+  def test_leduc_poker(self):
+    """Regression test of a real sequential move game."""
+    players = {
+        0: bots.RandomActionBot(num_actions=3),
+        1: bots.RandomActionBot(num_actions=3),
+    }
+    scenario = eval_arena.EvaluationScenario(
+        game_ctor=openspiel_proxy.OpenSpielProxy,
+        game_kwargs={"game": "leduc_poker"},
+        num_episodes=2,
+    )
+    arena = eval_arena.EvalArena(
+        players=players,
+        scenarios=scenario,
+        counter=None,
+        step_key=None,
+        logger=None,
+    )
+    arena.run_evaluation()
+
+  def test_sequential_move_return(self):
+    """Test a sequential move game."""
+    players = {
+        0: bots.RandomActionBot(num_actions=3),
+        1: bots.RandomActionBot(num_actions=3),
+    }
+
+    scenario = eval_arena.EvaluationScenario(
+        game_ctor=ScriptGame,
+        game_kwargs={
+            "timesteps": [
+                {0: worlds.TimeStep(step_type=worlds.StepType.FIRST, reward=1, observation=np.array([1]))},
+                {1: worlds.TimeStep(step_type=worlds.StepType.FIRST, reward=2, observation=np.array([2]))},
+                {0: worlds.TimeStep(step_type=worlds.StepType.MID, reward=3, observation=np.array([3]))},
+                {1: worlds.TimeStep(step_type=worlds.StepType.MID, reward=4, observation=np.array([4]))},
+                {0: worlds.TimeStep(step_type=worlds.StepType.LAST, reward=5, observation=np.array([5]))},
+                {1: worlds.TimeStep(step_type=worlds.StepType.LAST, reward=6, observation=np.array([6]))},
+            ],
+        },
+        num_episodes=2,
+    )
+    arena = eval_arena.EvalArena(
+        players=players,
+        scenarios=scenario,
+        counter=None,
+        step_key=None,
+        logger=None,
+    )
+    results = arena.run_evaluation()
+    tree_utils.assert_equals(
+        [
+            eval_arena.EpisodeResult(
+                episode_length=5,
+                episode_return={0: 9.0, 1: 12.0},
+            )
+        ],
+        results,
+    )
 
 
 if __name__ == "__main__":
