7967b24d8abab5b835cff89eca980cc776b47303
Max Smith <max.olan.smith@gmail.com>
2024-02-16 10:01:29 -0500

    Add Learner Policy & Update to API.

--- marl/services/__init__.py
+++ marl/services/__init__.py
@@ -1,6 +1,8 @@
 """Services that conduct routines common to MARL."""
 from .counter import Counter
 from .interfaces import Saveable, VariableSource, Worker
+from .learner_policy import LearnerPolicy
+from .learner_update import LearnerUpdate
 from .variable_client import VariableClient
 
 __all__ = (
@@ -8,5 +10,7 @@ __all__ = (
     "VariableClient",
     "Saveable",
     "VariableSource",
+    "LearnerPolicy",
+    "LearnerUpdate",
     "Worker",
 )
